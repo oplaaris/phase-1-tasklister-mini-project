31458aab1fcc2fff9a2cf70c2c8daf7a8bd469c3
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-03 21:57:13 +0300

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+function deleteTask(taskList, task) {
+  const index = taskList.indexOf(task);
+  if (index > -1) {
+    taskList.splice(index, 1);
+  }
+}
+function sortTasks(taskList, order) {
+  const priorityOrder = ['High', 'Medium', 'Low'];
+  taskList.sort((a, b) => {
+    const priorityA = priorityOrder.indexOf(a.priority);
+    const priorityB = priorityOrder.indexOf(b.priority);
+    if (order === 'ascending') {
+      return priorityA - priorityB;
+    } else {
+      return priorityB - priorityA;
+    }
+  });
+}
